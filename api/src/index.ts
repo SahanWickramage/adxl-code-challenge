5be6c9b567400421170577b283a0073f5050ae82
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-09-06 01:45:23 +1000

    task7 : established mongodb connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import connectDB from './config/MongoConnection';
 
 const app = express();
 
 const port = 3030;
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
