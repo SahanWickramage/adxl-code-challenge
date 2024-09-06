be2c96f7db5053e961db61c5807a6fa2e4035af5
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-09-06 18:11:43 +1000

    task8 : configured cors in api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import connectDB from './config/MongoConnection';
 import indexRouter from './route/IndexRoute';
+import cors from 'cors';
 
 const port = 3030;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/api/v1', indexRouter);
 
 connectDB();
