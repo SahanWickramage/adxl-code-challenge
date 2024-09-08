dc844ac8c732f8220a07e5b18c6de4b570b83fe8
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-09-08 16:28:16 +1000

    task10 : integrated ui with scan finding get endpoint

---
+++ dashboard/src/services/SecurityScanFindingService.ts
@@ -0,0 +1,19 @@
+import { ApiResponse } from "../types/ApiResponse";
+import { SecurityScanFinding } from "../types/SecurityScanFinding";
+import { SecurityScanFindingResponse } from "../types/SecurityScanFindingResponse";
+
+const url: string = "http://localhost:3030/api/v1";
+
+async function listAllByResultId(resultId: string) : Promise<SecurityScanFinding[] | undefined> {
+    
+    const response = await fetch(`${url}/findings/${resultId}`);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data: ApiResponse<SecurityScanFindingResponse> = await response.json();
+    return data.data?.securityScanFindings;
+}
+
+export { listAllByResultId };
